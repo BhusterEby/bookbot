26765436a8f825b8586a91bdd755539b19ff2684
BhusterEby <aiden.eby@gmail.com>
2025-04-26 20:45:50 -0500

    [Commit Catchup] updated main.py: prints out the number of words in the book

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ def get_book_text(fp):
     # Return: string containing the entire book
     return file_contents
 
+# Function: count all the words in the provided book
+def book_word_count(book_str):
+    # New list, containing all of the words generated using .split()
+    book_wrds = book_str.split()
+    # Count how many words there are in the list
+    word_numb = len(book_wrds)
+    # Return: number of words in the book
+    return word_numb
+
 # Function: main
 def main():
     # Get the story as a variable
     story_string = get_book_text("books/frankenstein.txt")
-    print(story_string)
+    # Get the word count
+    num_words = book_word_count(story_string)
+    print(f"{num_words} words found in the document")
 
 # Run
 main()
