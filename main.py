e98fabca0f29b21cceb5c7a7c2e03a5bc6fd9269
BhusterEby <aiden.eby@gmail.com>
2025-04-26 20:38:30 -0500

    [Commit Catchup] updated main.py: prints out the entire story of 'Frankenstein'

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+# Function: make a string of the provided .txt file (book) location
+def get_book_text(fp):
+    # Empty string, to store the book's contents in
+    file_contents = ""
+    # With block: opens the file
+    with open(fp) as f:
+        # Import the entire contents of the file to the string using .read()
+        file_contents = f.read()
+    # Return: string containing the entire book
+    return file_contents
+
+# Function: main
+def main():
+    # Get the story as a variable
+    story_string = get_book_text("books/frankenstein.txt")
+    print(story_string)
+
+# Run
+main()
